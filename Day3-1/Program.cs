5de4242d9bdbba98d02f68fcd261e48f39e79328
Fredrik Hult <fredrik.hult@outlook.com>
2024-12-07 21:14:22 +0100

    Improvements

--- Day3-1/Program.cs
+++ Day3-1/Program.cs
@@ -1,43 +1,42 @@
 ﻿using System.Diagnostics;
 
 var input = File.ReadAllText("input.txt").AsSpan();
-int index = 0;
-int sum = 0;
+var sum = 0;
 
 var sw = Stopwatch.StartNew();
 while (true)
 {
-	index = input.IndexOf("mul(");
+	var index = input.IndexOf("mul(");
 	if (index < 0) break;
-	input = input.Slice(index + 4);
+	input = input[(index + 4)..];
 
 	var length = 0;
 	while (length < input.Length && char.IsDigit(input[length]))
 		++length;
 
-	if (length == 0 || length > 3)
+	if (length is 0 or > 3)
 		continue;
 
 	var firstAsString = input.Slice(0, length);
 	int first = int.Parse(firstAsString);
 
-	input = input.Slice(length);
+	input = input[length..];
 	if (input[0] != ',')
 		continue;
 
-	input = input.Slice(1);
+	input = input[1..];
 
 	length = 0;
 	while (length < input.Length && char.IsDigit(input[length]))
 		++length;
 
-	if (length == 0 || length > 3)
+	if (length is 0 or > 3)
 		continue;
 
 	var secondAsString = input.Slice(0, length);
 	int second = int.Parse(secondAsString);
 
-	input = input.Slice(length);
+	input = input[length..];
 
 	if (input[0] != ')')
 		continue;
