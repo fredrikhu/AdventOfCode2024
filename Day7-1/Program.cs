6cd8a26eae1c25aefa2fd1dcc077b65c3c9af599
Fredrik Hult <fredrik.hult@outlook.com>
2024-12-17 00:40:06 +0100

    Day 7

---
+++ Day7-1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+var input = File.ReadAllLines("input.txt");
+var equations = input
+    .Select(line => line.Split(':'))
+    .Select(line => new Equation
+    {
+        Result = long.Parse(line[0]),
+        Operands = line[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray()
+    });
+
+long result = 0;
+
+var sw = Stopwatch.StartNew();
+
+foreach (var equation in equations)
+{
+    if (Solveable(equation))
+    {
+        result += equation.Result;
+    }
+}
+
+sw.Stop();
+
+Console.WriteLine($"{result} in {sw.Elapsed} ms");
+
+
+bool Solveable(Equation o)
+{
+    var results = new List<long> { o.Operands.First() };
+
+    foreach (var op in o.Operands.Skip(1))
+    {
+        results = results
+            .SelectMany(r => (long[])[r + op, r * op])
+            .ToList();
+    }
+    
+    return results.Any(r => r == o.Result);
+}
+
+class Equation
+{
+    public long Result { get; set; }
+    public long[] Operands { get; set; }
+}
